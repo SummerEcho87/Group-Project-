f499c5877ad6241d20e730ec78169b71d5fb68b2
David Flores <Dflores5200@gmail.com>
2022-08-07 11:16:41 -0500

    Update Crunchyroll.R

--- Code/Crunchyroll.R
+++ Code/Crunchyroll.R
@@ -1,6 +1,6 @@
 crunchyroll_titles <- read.csv('../Data/crunchyroll-titles.csv')
 head(crunchyroll_titles)
-crunchyroll.drop('id', 'description', 'age_certification', 'production_countries', 'tmdb_popularity', 'tmdb_score', axis=1, inplace=TRUE)
+
 #dropping rows that are not needed for data
 library('dplyr')
 mydata <- crunchyroll_titles
